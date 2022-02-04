0468f5a51e9c815e6b98062407892c3f04b47784
Can Çelik <68447137+can101@users.noreply.github.com>
2022-02-04 01:45:57 -0800

    secret verabiles
    deleting processs

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBmK4eZ2pOFiott6kq9wckQlSrs8lNkW40",
-    authDomain: "test-cfec6.firebaseapp.com",
-    projectId: "test-cfec6",
-    storageBucket: "test-cfec6.appspot.com",
-    messagingSenderId: "1022532599517",
-    appId: "1:1022532599517:web:ae5e843019a4d885432534"
+    //    firebase config veriables copy paste area
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
